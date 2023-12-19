1bb91b744992b1c361bb17aef142ed3523e585e0
Yoon <ysj0509@sookmyung.ac.kr>
2023-12-19 22:50:17 +0900

    REFACTOR
    변수를 반복해서 갱신하는 과정이 비효율적이라 생각하여, 리스트와 for문을 이용해 반복 구조를 개편했다.
    지폐와 동전은 for문의 i변수를 이용해 if문을 작성하여 분류했다.

--- Lab02/src/Lab02_03.java
+++ Lab02/src/Lab02_03.java
@@ -6,26 +6,19 @@ public class Lab02_03 {
 		Scanner scan = new Scanner(System.in);
 		System.out.print("금액을 입력하시오 >> ");
 		int inputSum = scan.nextInt();
+		int change;
 		
-		int bill = inputSum / 50000;
-		inputSum %= 50000;
-		System.out.println("오만원권 " + bill + "매");
-		bill = inputSum / 10000;
-		inputSum %= 10000;
-		System.out.println("만원권 " + bill + "매");
-		bill = inputSum / 1000;
-		inputSum %= 1000;
-		System.out.println("천원권 " + bill + "매");
-		bill = inputSum / 100;
-		inputSum %= 100;
-		System.out.println("백원 " + bill +"개");
-		bill = inputSum / 50;
-		inputSum %= 50;
-		System.out.println("오십원 " + bill +"개");
-		bill = inputSum / 10;
-		inputSum %= 10;
-		System.out.println("십원 " + bill +"개");
-		System.out.println("일원 " + inputSum +"개");
+		String bill[] = {"오만원권 ", "만원권 ", "천원권 ", "백원 ", "오십원 ", "십원 ", "일원 "};
+		int amount[] = {50000, 10000, 1000, 100, 50, 10, 1};
+		
+		for (int i = 0; i < 7; i++) {
+			change = inputSum / amount[i];
+			inputSum %= amount[i];
+			if (i < 3)
+				System.out.println(bill[i] + change + "매");
+			else
+				System.out.println(bill[i] + change + "개");
+		}
 		
 	}
 
