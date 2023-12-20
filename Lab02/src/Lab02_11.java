9eef0a2b5c4a3a85463c468ee52bbccd7494e6e5
Yoon <ysj0509@sookmyung.ac.kr>
2023-12-20 12:04:01 +0900

    REFACTOR : switch문의 expression과 case 수정
    굳이 month를 3으로 나누는 작업을 할 필요 없이, 그 값 자체를 대조하면 더 깔끔하게 작성할 수 있다.

--- Lab02/src/Lab02_11.java
+++ Lab02/src/Lab02_11.java
@@ -7,22 +7,18 @@ public class Lab02_11 {
 		System.out.print("달을 입력하세요 (1~12) >> ");
 		byte month = scan.nextByte();
 
-		switch (month / 3) {
-			case (1):
+		switch (month) {
+			case 3: case 4: case 5:
 				System.out.println("봄");
 				break;
-			case (2):
+			case 6: case 7: case 8:
 				System.out.println("여름");
 				break;
-			case (3):
+			case 9: case 10: case 11:
 				System.out.println("가을");
 				break;
-			case (4):
-			case (0):
-				if (month == 12 || month == 1 || month == 2)
-					System.out.println("겨울");
-				else
-					System.out.println("잘못 입력");
+			case 12: case 1: case 2:
+				System.out.println("겨울");
 				break;
 			default:
 				System.out.println("잘못 입력");
