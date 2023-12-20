fbe3656b193651d3472167ac8b4d112bfae2694c
Yoon <ysj0509@sookmyung.ac.kr>
2023-12-20 12:00:56 +0900

    REFACTOR : switch 문 사용
    각 계절이 3배수 month부터 시작된다는 점을 착안해, month를 3으로 정수 몫 나눗셈한 값으로 case를 분류하였다.
    허나 겨울의 경우, 몫이 4인 값은 12(월) 뿐만 아니라 13, 14, 15도 포함되며, 몫이 0인 값은 0 이하의 음수 또한
    포함된다는 부작용을 발견했다.
    이에 조건문을 사용해 12이거나, 1이거나, 2일 때만 "겨울"을 출력하도록 하였다.

--- Lab02/src/Lab02_11.java
+++ Lab02/src/Lab02_11.java
@@ -4,18 +4,29 @@ public class Lab02_11 {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		System.out.print("달을 입력하세요 (1~12)");
+		System.out.print("달을 입력하세요 (1~12) >> ");
 		byte month = scan.nextByte();
-		if (month >= 3 && month <= 5)
-			System.out.println("봄");
-		else if (month >= 6 && month <= 8)
-			System.out.println("여름");
-		else if (month >= 9 && month <= 11)
-			System.out.println("가을");
-		else if (month == 12 || month == 1 || month == 2)
-			System.out.println("겨울");
-		else
-			System.out.println("잘못 입력");
+
+		switch (month / 3) {
+			case (1):
+				System.out.println("봄");
+				break;
+			case (2):
+				System.out.println("여름");
+				break;
+			case (3):
+				System.out.println("가을");
+				break;
+			case (4):
+			case (0):
+				if (month == 12 || month == 1 || month == 2)
+					System.out.println("겨울");
+				else
+					System.out.println("잘못 입력");
+				break;
+			default:
+				System.out.println("잘못 입력");
+		}
 		scan.close();
 	}
 
