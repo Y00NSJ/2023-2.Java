a5ae4e725e79d39c049f8c7cd324c281cfe6ad4c
Yoon <ysj0509@sookmyung.ac.kr>
2023-12-23 20:12:55 +0900

    REFACTOR : 배수 저장용 array 생성
    3의 배수를 저장하는 multiple array를 추가하였다. 10개의 수를 입력 받으므로 그와 동일한 크기로 지정했다.
    for문을 10회 돌면서 매 회 입력받은 숫자를 intArray에 저장하고, 그 숫자가 3으로 나눠떨러질 경우
    multipleArray에 추가한다. multipleArray의 인덱스는 변수 j이며, 입력한 후에 j를 증가시킨다.
    해당 for문이 끝난 후 j값은 값이 할당된 원소의 개수와 같다.
    출력시엔 (j-1)만큼 for문을 돌면서 출력한다.
    ** 최종 j값은 배수배열의 인덱스가 아닌 배수배열의 크기와 같다는 사실을 잊었다.
    ** 배수 출력 시 배수 배열을 입력 배열로 잘못 기재해 출력 오류가 났었다.

--- Lab03/src/Lab03_05.java
+++ Lab03/src/Lab03_05.java
@@ -5,16 +5,22 @@ public class Lab03_05 {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		int[] intArray = new int[10];
+		int[] multipleArray = new int[10];
+		int j = 0;
 		
 		System.out.print("양의 정수 10개를 입력하시오 >> ");
+		
 		for (int i = 0; i < 10; i++) {
 			intArray[i] = scan.nextInt();
+			if (intArray[i] % 3 == 0) {
+				multipleArray[j] = intArray[i];
+				j++;
+			}
 		}
 		
 		System.out.print("3의 배수는 ");
-		for (int i = 0; i < 10; i++)
-			if (intArray[i] % 3 == 0)
-				System.out.print(intArray[i] + " ");
+		for (int i = 0; i < j; i++)
+			System.out.print(multipleArray[i] + " ");
 
 		scan.close();
 	}
