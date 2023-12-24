a14449e472c7e60b9bae55db592c1ad1c8baf281
Yoon <ysj0509@sookmyung.ac.kr>
2023-12-24 18:57:07 +0900

    FEAT : 월 입력 기능 추가, 객체 배열 추가
    객체 배열을 추가했으므로 input() 메소드에선 객체 생성을 하지 않고 바로 Day의 set() 메소드를 실행한다.
    월 입력 기능을 추가해서 초기에 몇 월의 계획을 세울 것인지 지정할 수 있다.
    ?? : 스캐너를 하나만 쓸 순 없는가?

--- Lab04/src/Lab04_07.java
+++ Lab04/src/Lab04_07.java
@@ -17,8 +17,17 @@ class Day {
 }
 
 class MonthSchedule {
-	Day [] schedule = new Day [31];
+	int mDays;
+	Day [] schedule;
 	Scanner scan = new Scanner(System.in);
+	
+	public MonthSchedule(int mDays) {
+		this.mDays = mDays;
+		schedule = new Day[mDays];
+		for (int i = 0; i < schedule.length; i++) {
+			schedule[i] = new Day();
+		}
+	}
 	
 	public void run() {
 		System.out.println("<이번 달 스케줄 관리 프로그램>");
@@ -46,28 +55,58 @@ class MonthSchedule {
 	public void input() {
 		System.out.print("날짜 (1 ~ 31) ? >> ");
 		int day = scan.nextInt();
-		schedule[day] = new Day();
+		if (day < 0 || day > mDays) {
+			System.out.println("날짜를 잘못 입력하였습니다!");
+			return;
+		}
 		System.out.print("할 일(빈 칸 없이 입력) >> ");
-		schedule[day].set(scan.next());
+		schedule[day - 1].set(scan.next());
 	}
 	
 	public void view() {
 		System.out.print("날짜 (1 ~ 31) ? >> ");
 		int searchday = scan.nextInt();
 		System.out.print(searchday + "일의 할 일은 ");
-		schedule[searchday].show();
+		schedule[searchday - 1].show();
 	}
 	
 	public void finish() {
 		System.out.println("프로그램을 종료합니다.");
+		scan.close();
 	}
 }
 
 public class Lab04_07 {
 
 	public static void main(String[] args) {
-		MonthSchedule ms1 = new MonthSchedule();
-		ms1.run();
+		Scanner scan = new Scanner(System.in);
+		while (true) {
+			System.out.print("몇 월의 계획을 세우시겠습니까? (숫자로 입력) >> ");
+			int month = scan.nextInt();
+			switch (month) {
+				case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+					MonthSchedule oddMonth = new MonthSchedule(31);
+					oddMonth.run();
+					scan.close();
+					break;
+				case 4: case 6: case 9: case 11:
+					MonthSchedule evenMonth = new MonthSchedule(30);
+					evenMonth.run();
+					scan.close();
+					break;
+				case 2:
+					MonthSchedule feb = new MonthSchedule(28);
+					feb.run();
+					scan.close();
+					break;
+				default:
+					System.out.println("1~12 사이의 숫자를 입력하세요.");
+			}
+			if (month < 1 || month > 12)
+				continue;
+			else
+				break;
+		}
 	}
 
 }
