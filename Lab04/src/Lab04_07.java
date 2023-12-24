3a63366c138e5dec9e923fc09a3676e6cad37757
Yoon <ysj0509@sookmyung.ac.kr>
2023-12-24 18:34:34 +0900

    REFACTOR : 두 개의 클래스를 이용해 설계
    프로그램을 실행하는 클래스인 MonthSchedule에는 input(), view(), finish(), run() 메소드가
    있다.

--- Lab04/src/Lab04_07.java
+++ Lab04/src/Lab04_07.java
@@ -16,32 +16,24 @@ class Day {
 	}
 }
 
-public class Lab04_07 {
-
-	public static void main(String[] args) {
-		Day [] schedule = new Day [31];
-		
-		Scanner scan = new Scanner(System.in);
+class MonthSchedule {
+	Day [] schedule = new Day [31];
+	Scanner scan = new Scanner(System.in);
+	
+	public void run() {
 		System.out.println("<이번 달 스케줄 관리 프로그램>");
 		while (true) {
 			System.out.print("\n할 일 (입력 = 1, 보기 = 2, 끝내기 = 3) >> ");
 			int userMenu = scan.nextInt();
 			switch (userMenu) {
 				case 1:
-					System.out.print("날짜 (1 ~ 31) ? >> ");
-					int day = scan.nextInt();
-					schedule[day] = new Day();
-					System.out.print("할 일(빈 칸 없이 입력) >> ");
-					schedule[day].set(scan.next());
+					this.input();
 					break;
 				case 2:
-					System.out.print("날짜 (1 ~ 31) ? >> ");
-					int searchday = scan.nextInt();
-					System.out.print(searchday + "일의 할 일은");
-					schedule[searchday].show(); 
+					this.view();
 					break;
 				case 3:
-					System.out.println("프로그램을 종료합니다.");
+					this.finish();
 					break;
 				default:
 					System.out.println("1, 2, 3 중 하나를 입력하세요.");
@@ -49,6 +41,33 @@ public class Lab04_07 {
 			if (userMenu == 3)
 				break;
 		}
+	}
+		
+	public void input() {
+		System.out.print("날짜 (1 ~ 31) ? >> ");
+		int day = scan.nextInt();
+		schedule[day] = new Day();
+		System.out.print("할 일(빈 칸 없이 입력) >> ");
+		schedule[day].set(scan.next());
+	}
+	
+	public void view() {
+		System.out.print("날짜 (1 ~ 31) ? >> ");
+		int searchday = scan.nextInt();
+		System.out.print(searchday + "일의 할 일은 ");
+		schedule[searchday].show();
+	}
+	
+	public void finish() {
+		System.out.println("프로그램을 종료합니다.");
+	}
+}
+
+public class Lab04_07 {
+
+	public static void main(String[] args) {
+		MonthSchedule ms1 = new MonthSchedule();
+		ms1.run();
 	}
 
 }
