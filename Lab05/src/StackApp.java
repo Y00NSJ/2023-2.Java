52d9bdb250277eb5e18b9c06448bd57506a9f89e
Yoon <ysj0509@sookmyung.ac.kr>
2024-01-01 03:02:05 +0900

    스택 가장 밑 원소가 출력되지 않고 있음

---
+++ Lab05/src/StackApp.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class StackApp {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.print("총 스택 저장 공간의 크기 입력 >> ");
+		StringStack st = new StringStack(scan.nextInt());
+		
+		while (true) {
+			System.out.print("문자열 입력 >> ");
+			String stackInput = scan.next();
+			if (stackInput.equals("그만")) 
+				break;
+			if (st.push(stackInput) == false)
+				System.out.println("스택이 꽉 차서 푸시 불가!");
+		}
+		System.out.print("스택에 저장된 모든 문자열 팝 : ");
+		System.out.println(st.length());
+		for (int i = 1; i <= st.length(); i++)
+			System.out.print(st.pop() + " ");
+		scan.close();
+	}
+
+}
