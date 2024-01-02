188dd7ba78ef3f03c75089ea01bc79cc7a2814c9
Yoon <ysj0509@sookmyung.ac.kr>
2024-01-02 13:20:16 +0900

    initial code

---
+++ Lab06/src/Lab06_01.java
@@ -0,0 +1,15 @@
+
+public class Lab06_01 {
+
+	public static void main(String[] args) {
+		MyPoint p = new MyPoint(3, 50);
+		MyPoint q = new MyPoint(4, 50);
+		System.out.println(p);
+		System.out.println(q);
+		if (p.equals(q))
+			System.out.println("같은 점");
+		else
+			System.out.println("다른 점");
+	}
+
+}
---
+++ Lab06/src/MyPoint.java
@@ -0,0 +1,14 @@
+
+public class MyPoint {
+	private int x, y;
+	public MyPoint(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	public boolean equals(MyPoint p) {
+		if (this.x == p.x && this.y == p.y)
+			return true;
+		else
+			return false;
+	}
+}
