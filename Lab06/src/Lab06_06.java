845a745588cab8127da6f3eec8828ba965c67e99
Yoon <ysj0509@sookmyung.ac.kr>
2024-01-08 06:10:38 +0900

    fix : nextLine으로 엔터 키 받기
    초 정보가 갱신되지 않고 있음

--- Lab06/src/Lab06_06.java
+++ Lab06/src/Lab06_06.java
@@ -14,17 +14,17 @@ public class Lab06_06 {
 		System.out.println("10초에 가까운 사람이 이기는 게임입니다.");
 		for (int i = 0; i < name.length; i++) {
 			System.out.print(name[i] + " 시작 <Enter>키 >> ");
-			scan.next();
+			scan.nextLine();
 			currentSec = time.get(Calendar.SECOND);
-			System.out.print("현재 초 시간 = " + currentSec + "\n10초 예상 후 <Enter> 키 >> ");
-			scan.next();
+			System.out.print("	현재 초 시간 = " + currentSec + "\n" + "10초 예상 후 <Enter> 키 >> ");
+			scan.nextLine();
 			afterSec = time.get(Calendar.SECOND);
-			System.out.print("현재 초 시간 = " + afterSec);
-			result[i] = (currentSec < afterSec)? afterSec - currentSec: Math.abs(currentSec - (afterSec + 10));
+			System.out.println("	현재 초 시간 = " + afterSec);
+			result[i] = (currentSec <= afterSec)? afterSec - currentSec: Math.abs(currentSec - (afterSec + 60));
 			
 		}
 		
-		System.out.println(name[0] + "의 결과 " + result[0] + ", " + name[1] + "의 결과 " + result[1]);
+		System.out.print(name[0] + "의 결과 " + result[0] + ", " + name[1] + "의 결과 " + result[1]);
 		if (result[0] > result [1])
 			System.out.println(", 승자는 " + name[0]);
 		else if (result[0] < result [1])
