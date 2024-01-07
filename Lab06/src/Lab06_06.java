a8f98ce6e1c3159f6a0e6681f5d4fde0ab6447ba
Yoon <ysj0509@sookmyung.ac.kr>
2024-01-08 07:15:52 +0900

    refactor : 객체 생성 형태

--- Lab06/src/Lab06_06.java
+++ Lab06/src/Lab06_06.java
@@ -1,41 +1,60 @@
 import java.util.Calendar;
 import java.util.Scanner;
 
-public class Lab06_06 {
-
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		String[] name = {"황기태", "이재문"};
-		int[] result = new int [name.length];
+class Player {
+	Scanner scan = new Scanner(System.in);
+	private String name;
+	
+	public Player(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public int game() {
+		System.out.print(name + " 시작 <Enter>키 >> ");
+		scan.nextLine();
+		Calendar time = Calendar.getInstance();
+		int currentSec = time.get(Calendar.SECOND);
+		System.out.print("	현재 초 시간 = " + currentSec + "\n" + "10초 예상 후 <Enter> 키 >> ");
+		scan.nextLine();
 		
-		int currentSec = 0;
-		int afterSec = 0;
+		// 캘린더 다시 받아오기: 새로 해야 현재초를 받아올 수 있음
+		time = Calendar.getInstance();
+		int afterSec = time.get(Calendar.SECOND);
+		System.out.println("	현재 초 시간 = " + afterSec);
+		int result = (currentSec <= afterSec)? afterSec - currentSec: afterSec + 60 - currentSec;
+		return result;
+	}
+	
+	//public int game()
+}
+
+public class Lab06_06 {
+	public void startGame() {
+		Player[] p = new Player[2];
+		p[0] = new Player("황기태");
+		p[1] = new Player("이재문");
 		
 		System.out.println("10초에 가까운 사람이 이기는 게임입니다.");
-		for (int i = 0; i < name.length; i++) {
-			System.out.print(name[i] + " 시작 <Enter>키 >> ");
-			scan.nextLine();
-			Calendar time = Calendar.getInstance();
-			currentSec = time.get(Calendar.SECOND);
-			System.out.print("	현재 초 시간 = " + currentSec + "\n" + "10초 예상 후 <Enter> 키 >> ");
-			scan.nextLine();
-			
-			// 캘린더 다시 받아오기: 새로 해야 현재초를 받아올 수 있음
-			time = Calendar.getInstance();
-			afterSec = time.get(Calendar.SECOND);
-			System.out.println("	현재 초 시간 = " + afterSec);
-			result[i] = (currentSec <= afterSec)? afterSec - currentSec: afterSec + 60 - currentSec;
-			
-		}
 		
-		System.out.print(name[0] + "의 결과 " + result[0] + ", " + name[1] + "의 결과 " + result[1]);
-		if (Math.abs(result[0] - 10) < Math.abs(result[1] - 10))
-			System.out.println(", 승자는 " + name[0]);
-		else if (Math.abs(result[0] - 10) > Math.abs(result[1] - 10))
-			System.out.println(", 승자는 " + name[1]);
+		int diff1 = p[0].game();
+		int diff2 = p[1].game();
+		
+		System.out.print(p[0].getName() + "의 결과 " + diff1 + ", " + p[1].getName() + "의 결과 " + diff2);
+		if (Math.abs(diff1 - 10) < Math.abs(diff2 - 10))
+			System.out.println(", 승자는 " + p[0].getName());
+		else if (Math.abs(diff1 - 10) > Math.abs(diff2 - 10))
+			System.out.println(", 승자는 " + p[1].getName());
 		else
 			System.out.println(", 무승부입니다.");
-		scan.close();
+	}
+	
+	public static void main(String[] args) {
+		Lab06_06 game = new Lab06_06();
+		game.startGame();
 	}
 
 }
