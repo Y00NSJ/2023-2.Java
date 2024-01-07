dc69f35f2f65e56cb57bb1a2055f383a54878cef
Yoon <ysj0509@sookmyung.ac.kr>
2024-01-08 06:33:00 +0900

    fix
    ** getInstance를 재호출해야 갱신됨!
    - 10초에 가까운 사람이 이기는 것.

--- Lab06/src/Lab06_06.java
+++ Lab06/src/Lab06_06.java
@@ -7,7 +7,7 @@ public class Lab06_06 {
 		Scanner scan = new Scanner(System.in);
 		String[] name = {"황기태", "이재문"};
 		int[] result = new int [name.length];
-		Calendar time = Calendar.getInstance();
+		
 		int currentSec = 0;
 		int afterSec = 0;
 		
@@ -15,19 +15,23 @@ public class Lab06_06 {
 		for (int i = 0; i < name.length; i++) {
 			System.out.print(name[i] + " 시작 <Enter>키 >> ");
 			scan.nextLine();
+			Calendar time = Calendar.getInstance();
 			currentSec = time.get(Calendar.SECOND);
 			System.out.print("	현재 초 시간 = " + currentSec + "\n" + "10초 예상 후 <Enter> 키 >> ");
 			scan.nextLine();
+			
+			// 캘린더 다시 받아오기: 새로 해야 현재초를 받아올 수 있음
+			time = Calendar.getInstance();
 			afterSec = time.get(Calendar.SECOND);
 			System.out.println("	현재 초 시간 = " + afterSec);
-			result[i] = (currentSec <= afterSec)? afterSec - currentSec: Math.abs(currentSec - (afterSec + 60));
+			result[i] = (currentSec <= afterSec)? afterSec - currentSec: afterSec + 60 - currentSec;
 			
 		}
 		
 		System.out.print(name[0] + "의 결과 " + result[0] + ", " + name[1] + "의 결과 " + result[1]);
-		if (result[0] > result [1])
+		if (Math.abs(result[0] - 10) < Math.abs(result[1] - 10))
 			System.out.println(", 승자는 " + name[0]);
-		else if (result[0] < result [1])
+		else if (Math.abs(result[0] - 10) > Math.abs(result[1] - 10))
 			System.out.println(", 승자는 " + name[1]);
 		else
 			System.out.println(", 무승부입니다.");
