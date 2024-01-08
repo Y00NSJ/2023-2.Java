68bec52a8d00ec7e6ef0b0c0a53a37c5cb014f4d
Yoon <ysj0509@sookmyung.ac.kr>
2024-01-08 10:19:23 +0900

    reference code
    (i+1, 문자열길이) + (0, i+1)로 하면,
    마지막 i값이 (문자열 최후 인덱스) 이므로 문자열 전체가 출력되며 끝난다.

--- Lab06/src/Lab06_08.java
+++ Lab06/src/Lab06_08.java
@@ -6,10 +6,9 @@ public class Lab06_08 {
 		Scanner scan = new Scanner(System.in);
 		System.out.println("문자열을 이력하세요. 빈칸/영어/한글 모두 가능");
 		String st = scan.nextLine();
-		for (int i = 1; i < st.length(); i++) {
-			System.out.println(st.substring(i) + st.substring(0, i));
+		for (int i = 0; i < st.length(); i++) {
+			System.out.println(st.substring(i+1,st.length()) + st.substring(0,i+1));
 		}
-		System.out.println(st);
 		scan.close();
 	}
 
