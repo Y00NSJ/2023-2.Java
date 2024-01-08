2909416afcc2a7a1e500cf0c6764331bcf69ac6d
Yoon <ysj0509@sookmyung.ac.kr>
2024-01-08 10:22:13 +0900

    실습 코드
    맨 앞 글자만 잘라 맨 뒤로 보내어 문자열 변수 st를 매번 갱신한다.

--- Lab06/src/Lab06_08.java
+++ Lab06/src/Lab06_08.java
@@ -7,7 +7,14 @@ public class Lab06_08 {
 		System.out.println("문자열을 이력하세요. 빈칸/영어/한글 모두 가능");
 		String st = scan.nextLine();
 		for (int i = 0; i < st.length(); i++) {
-			System.out.println(st.substring(i+1,st.length()) + st.substring(0,i+1));
+		// 현재 문자열에서 첫글자 가져오기
+			String initial = st.substring(0, 1);
+		// 나머지 글자 다 가져오기
+			String others = st.substring(1);
+		// 문자열 = 나머지글자 + 첫글자
+			st = others + initial;
+		// 문자열 출력
+			System.out.println(st);
 		}
 		scan.close();
 	}
