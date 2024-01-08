f871c4e13c36ef3662467adf28ec6e104374b9b0
Yoon <ysj0509@sookmyung.ac.kr>
2024-01-08 10:05:27 +0900

    initial code

---
+++ Lab06/src/Lab06_08.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Lab06_08 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("문자열을 이력하세요. 빈칸/영어/한글 모두 가능");
+		String st = scan.nextLine();
+		for (int i = 1; i < st.length(); i++) {
+			System.out.println(st.substring(i) + st.substring(0, i));
+		}
+		System.out.println(st);
+		scan.close();
+	}
+
+}
