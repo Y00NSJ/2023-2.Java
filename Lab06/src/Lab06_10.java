4ac3f467fcdfd39768172c280a662b4836398268
Yoon <ysj0509@sookmyung.ac.kr>
2024-01-09 16:18:29 +0900

    refactor : sample code
    - setting 메소드 추가(분리) : 플레이어 등록용
    - boolean 변수를 game 메소드 내에서 생성
    - 난수를 변수 세 개를 사용해 생성

--- Lab06/src/Lab06_10.java
+++ Lab06/src/Lab06_10.java
@@ -12,38 +12,45 @@ class Person {
 }
 
 public class Lab06_10 {
+	Scanner scan = new Scanner(System.in);
+	Random rd = new Random();
+	int [] rdNum = new int[3];
+	Person [] twoP = new Person [2];
 	
-	public void game() {
-		Scanner scan = new Scanner(System.in);
-		Random rd = new Random();
-		int [] rdNum = new int[3];
-		boolean isEnd = false;
-		Person [] twoP = new Person [2];
+	public void setting() {	//
 		for (int i = 0; i < twoP.length; i++) {
 			System.out.print((i+1) + "번째 선수 이름 >> ");
 			twoP[i] = new Person(scan.nextLine());
 		}
-		
+	}
+	
+	
+	public void game() {
+		setting();
+		boolean isEnd = false;	//
 		while (true) {
 			for (int i = 0; i < twoP.length; i++) {
 				System.out.print("[" + twoP[i].getName() + "] : <Enter>");
 				scan.nextLine();
 				System.out.print("\t");
-				for (int j = 0; j < rdNum.length; j++) {
-					rdNum[j] = rd.nextInt(3) + 1;
-					System.out.print(rdNum[j] + "   ");
-				}
-				if (rdNum[0] == rdNum[1] && rdNum[1] == rdNum[2]) {
+				
+				int rdNum1 = rd.nextInt(3) + 1;
+				int rdNum2 = rd.nextInt(3) + 1;
+				int rdNum3 = rd.nextInt(3) + 1;
+				System.out.print(rdNum1 + "   " + rdNum2 + "   " + rdNum3 + "   ");
+				
+				if (rdNum1 == rdNum2 && rdNum2 == rdNum3) {
 					System.out.println(twoP[i].getName() + "님이 이겼습니다!");
-					scan.close();
 					isEnd = true;
 					break;
 				}
 				else
 					System.out.println("아쉽군요!");
 			}
-			if (isEnd)
+			if (isEnd) {
+				scan.close();
 				break;
+			}
 		}
 	}
 	
