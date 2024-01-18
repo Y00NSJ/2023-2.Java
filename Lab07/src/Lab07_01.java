1c24b5ec2d2a93636db203be3a186fe0902a844f
Yoon <ysj0509@sookmyung.ac.kr>
2024-01-18 12:08:11 +0900

    Refactor : Sample Code
    - 벡터 요소 및 최댓값 출력을 메소드로 분리하였다.
    - 수가 하나도 없을 경우의 행동문을 추가하였다.
    - 입력 받은 요소를 벡터에 추가할 때 else 키워드를 삭제하였다.

--- Lab07/src/Lab07_01.java
+++ Lab07/src/Lab07_01.java
@@ -2,25 +2,41 @@ import java.util.Scanner;
 import java.util.Vector;
 
 public class Lab07_01 {
+	public static void printMax(Vector<Integer> v) {
+		int max = v.get(0);
+		for (int i = 1; i < v.size(); i++) {
+			if (max < v.get(i))
+				max = v.get(i);
+		}
+		System.out.println("가장 큰 수는 " + max);
+	}
+	
+	public static void printVect(Vector<Integer> v) {
+		for (Integer printNum  : v)
+			System.out.printf("%d ", printNum);
+		System.out.println();
+	}
+	
 	public static void main(String[] args) {
 		Vector<Integer> nums = new Vector<>();
 		Scanner scan = new Scanner(System.in);
-		int max = -1;
-		int num;
+		
 		System.out.print("양의 정수 입력 (빈 칸으로 분리, -1이 입력될 때까지)>> ");
 		while (true) {
-			num = scan.nextInt();
+			int num = scan.nextInt();
 			if (num == -1)
 				break;
-			else {
-				nums.add(num);
-				if (max < num)
-					max = num;
-			}
+			nums.add(num);
 		}
-		for (Integer printNum  : nums)
-			System.out.printf("%d ", printNum);
-		System.out.println("\n가장 큰 수는 " + max);
+		
+		if (nums.size() == 0) {
+			System.out.println("수가 하나도 없음");
+			scan.close();
+			return;
+		}
+		
+		printVect(nums);
+		printMax(nums);
 		scan.close();
 	}
 
