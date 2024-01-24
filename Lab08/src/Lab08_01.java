44b168db98f8cfe5389255bc40d6436770a4a38d
Yoon <ysj0509@sookmyung.ac.kr>
2024-01-24 09:13:20 +0900

    Refactor : Sample Code
    - 파일 클래스 활용
    - scan.nextLine()이 못 읽는 엔터를 넣기 위해 "\r\n"

--- Lab08/src/Lab08_01.java
+++ Lab08/src/Lab08_01.java
@@ -3,23 +3,27 @@ import java.util.Scanner;
 
 public class Lab08_01 {
 	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		FileWriter fin;
-		System.out.println("전화번호 입력 프로그램입니다.");
+		FileWriter fin = null;
+		File f = new File("C:\\Users\\user\\temp\\phone.txt");
+		
 		try {
-			fin = new FileWriter("C:\\Users\\user\\temp\\phone.txt");
+			fin = new FileWriter(f);
+			Scanner scan = new Scanner(System.in);
+			
+			System.out.println("전화번호 입력 프로그램입니다.");
 			while (true) {
 				System.out.print("이름 전화번호 >> ");
 				String line = scan.nextLine();
 				if (line.equals("그만"))
 					break;
-				fin.write(line);
+				fin.write(line + "\r\n");
 			}
+			System.out.println(f.getPath() + "에 저장하였습니다.");
 			scan.close();
 			fin.close();
-			System.out.println("저장하였습니다.");
 		}
 		catch (IOException e) {
+			e.printStackTrace(); //에러 발생 근원 찾아 단계별 출력
 			System.out.println("작성에 실패하였습니다.");
 		}
 	}
