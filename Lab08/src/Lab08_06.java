bb1e54fbfb0abd2aa51fab5fd2de761154fa2667
Yoon <ysj0509@sookmyung.ac.kr>
2024-01-27 17:58:48 +0900

    f1 안 읽히는 문제 원인 분석
    read(char[] cbuf)는 읽은 '개수'를 리턴한다.
    f2는 왜 읽히는가?

--- Lab08/src/Lab08_06.java
+++ Lab08/src/Lab08_06.java
@@ -24,22 +24,20 @@ public class Lab08_06 {
 			FileWriter appendFile = new FileWriter("appended.txt");
 			char[] buf1 = new char[1024];
 			int f1Count = 0, f2Count = 0, idx1 = -1;
-			
 			while (f1Count != -1) {
 				f1Count = fin1.read(buf1);
-				//System.out.println(f1Count);
+				System.out.println(f1Count);
 				idx1++;
+				//fin2.read(buf1, f1Count-1, 1024);
 			}
 			while (f2Count != -1) {
 				f2Count = fin2.read(buf1, idx1, 512);
 			}
-			
 			for (int i = 0; i < buf1.length; i++) {
 				appendFile.write(buf1[i]);
 				if (i == idx1 - 1)
 					appendFile.write("\r\n");
 			}
-			
 			fin1.close();
 			fin2.close();
 			appendFile.close();
