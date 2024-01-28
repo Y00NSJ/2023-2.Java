c6fc94e5d8ab232cf439774abc6ad9bef372f75c
Yoon <ysj0509@sookmyung.ac.kr>
2024-01-28 12:30:04 +0900

    assignment code
    appendFile(Filereader r, FileWriter w)
    r에서 한 글자를 읽어 char로 캐스팅한 후 w에 작성: -1 만날 때까지
    위 메소드를 f1, f2에서 두 번 시행

--- Lab08/src/Lab08_06.java
+++ Lab08/src/Lab08_06.java
@@ -5,48 +5,50 @@ public class Lab08_06 {
 	
 	public void run() {
 		Scanner scan = new Scanner(System.in);
+		File f = new File("appended.txt");
+		FileReader r = null;
+		FileWriter w = null;
 		System.out.println("전체 경로명이 아닌 파일명만 입력하는 경우, 파일은 프로젝트 폴더에 있어야 합니다.");
 		System.out.print("첫 번째 파일명을 입력하세요 >> ");
 		String file1 = scan.next();
 		System.out.print("두 번째 파일명을 입력하세요 >> ");
 		String file2 = scan.next();
 		
-		appendFile(file1, file2);
+		try {
+			w = new FileWriter(f);
+			r = new FileReader(file1);
+			appendFile(r, w);
+			w.write("\r\n");
+			r = new FileReader(file2);
+			appendFile(r, w);
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			System.out.println("프로젝트 폴더 밑 appended.txt 파일에 저장했습니다.");
+			scan.close();
+			if (r != null)
+				try {
+					r.close();
+				} catch (IOException e) {
+					
+				}
+			if (w != null)
+				try {
+					w.close();
+				} catch (IOException e) {
+					
+				}
+		}
 		
-		System.out.println("프로젝트 폴더 밑 appended.txt 파일에 저장했습니다.");
-		scan.close();
+		
 	}
 	
-	public void appendFile(String f1, String f2) {
+	public void appendFile(FileReader f1, FileWriter f2) {
+		int i = 0;
 		try {
-			FileReader fin1 = new FileReader(f1);
-			FileReader fin2 = new FileReader(f2);
-			FileWriter appendFile = new FileWriter("appended.txt");
-			char[] buf1 = new char[1024];
-			int f1Count = 0, f2Count = 0, idx1 = 0;
-			
-			while ((f1Count = fin1.read()) != -1) {
-				buf1[idx1] = (char)f1Count;
-				//System.out.println(f1Count);
-				idx1++;
-				//fin2.read(buf1, f1Count-1, 1024);
+			while ((i = f1.read()) != -1) {
+				f2.write((char)i);
 			}
-			
-			while (f2Count != -1) {
-				f2Count = fin2.read(buf1, idx1, 512);
-			}
-			
-			for (int i = 0; i < buf1.length; i++) {
-				appendFile.write(buf1[i]);
-				if (i == idx1 - 1)	//인덱스?
-					appendFile.write("\r\n");
-			}
-			
-			fin1.close();
-			fin2.close();
-			appendFile.close();
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
