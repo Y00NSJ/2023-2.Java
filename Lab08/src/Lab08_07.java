9e8a80e5f9966ba10f5e2bb617cf28ab03e6f4c4
Yoon <ysj0509@sookmyung.ac.kr>
2024-01-28 18:47:40 +0900

    Refactor : sample code
    1. buf[] 단위로 1byte씩 읽고, 리턴받는 '읽은 바이트 수'를 statAmount에 저장
    2 - a. statAmount가 -1이면
    	10번째 *이 안찍혔을 경우 찍고 종료
    2. statAmount > 0이면 파일쓰기
    3. progress에 읽은 바이트 수 저장
    	progress가 status 넘으면 = 10% 이상일 경우
    	* 찍고 변수 초기화

--- Lab08/src/Lab08_07.java
+++ Lab08/src/Lab08_07.java
@@ -5,28 +5,53 @@ public class Lab08_07 {
 	public void copyFile(File f) {
 		BufferedInputStream fin = null;
 		BufferedOutputStream fout = null;
-		long fSize = f.length();
-		int status = (int)(fSize / 10);
 		try {
-			fin = new BufferedInputStream(new FileInputStream(f), status);
-			fout = new BufferedOutputStream(new FileOutputStream(f + "_copied.png"), status);
-			
-			int c;
+			fin = new BufferedInputStream(new FileInputStream(f));
+			fout = new BufferedOutputStream(new FileOutputStream(f + "_copied.png"));
+			long status = f.length() / 10;
+			long progress = 0; // 10% 될 때까지 읽은 누적 바이트 수
+
+			byte[] buf = new byte[1024];
 			int statAmount = 0;
-			while ((c = fin.read()) != -1) {
-				fout.write((char)c);
-				statAmount++;
-				if (statAmount % status == 0)
+			while (true) {
+				statAmount = fin.read(buf, 0, buf.length);
+				if (statAmount == -1) {	//파일 끝
+					if (progress > 0)	//지난 번에 읽었으나 10%에 도달하지 않아 *이 출력되지 않은 경우
+						System.out.print("*");
+					break;
+				}
+				if (statAmount > 0)
+					fout.write(buf, 0, statAmount);
+				
+				progress += statAmount; // 파일 크기 10% 될 때까지 읽은 누적 바이트 수
+				if (progress >= status) {
 					System.out.print("*");
+					progress = 0; //초기화
+				}
 			}
 			
 		} catch (IOException e) {
 			e.getStackTrace();
+		} finally {
+			if (fin != null)
+				try {
+					fin.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			if (fout != null)
+				try {
+					fout.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 		}
 	}
 	
 	public void run() {
-		System.out.println("a.jpg를 복사합니다." + "\n" + "10%마다 *를 출력합니다.");
+		System.out.println("a.jpg를 복사합니다. \n10%마다 *를 출력합니다.");
 		File alien = new File("a.png");
 		copyFile(alien);
 	}
